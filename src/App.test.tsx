85ebc74da0a290a4517ab1cecbce670845a2c832
Piyush Pradhan <piyushpradhan3.14@gmail.com>
2023-02-05 16:49:52 +0530

    Setup pre-commit hooks and eslint

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   const { getByText } = render(
     // <Provider store={store}>
-      <App />
+    <App />
     // </Provider>
   );
 
